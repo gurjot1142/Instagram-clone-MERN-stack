5d65f9c1fe3600398a78ecc31446da98e1911bd5
gurjot1142 <58973468+gurjot1142@users.noreply.github.com>
2021-02-27 11:48:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const mongoose  = require('mongoose')
+const PORT = process.env.PORT || 5000
+const {MONGOURI} = require('./config/keys')
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+mongoose.connection.on('connected',()=>{
+    console.log("connected to mongo yeahh")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
+require('./models/user')
+require('./models/post')
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+app.use(require('./routes/user'))
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports={
+   MONGOURI: "mongodb+srv://gurjot:CZmeMQWUk75QAVqV@cluster0.6p1l6.mongodb.net/<dbname>?retryWrites=true&w=majority",
+   JWT_SECRET: "a1b2c3d4e5",
+   SENDGRID_API_KEY:'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'
+}
